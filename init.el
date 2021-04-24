b55c3ac7b544d67b9fd50e8107f3dcf299d1321b
James Mosedale <jmdale@protonmail.com>
2021-04-24 02:50:55 -0500

    init

---
+++ init.el
@@ -0,0 +1,38 @@
+;; James Mosedale Emacs config (very minimal)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(package-selected-packages '(evil projectile rust-mode tuareg treemacs slime flycheck)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'evil)
+(evil-mode 1)
+
+(global-flycheck-mode)
+
+;; C/C++
+(setq-default c-basic-offset 4
+	      c-default-style "k&r"
+	      indent-tabs-mode nil)
+
+(setq-default flycheck-gcc-args "-std=c++17")
+
+(global-display-line-numbers-mode)
+(setq inferior-lisp-program "sbcl")
+
+(setq split-width-threshold nil)
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
